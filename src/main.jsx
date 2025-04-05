dd1c4d14382ef290379e2db008be7df277a74718
Luna-bf <lburattifayolle@gmail.com>
2025-04-05 14:45:15 +0200

    Added a Count componant

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Count from './Count.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode> {/*Le mode strict permet d'afficher les erreurs en cas de problème*/}
     <App />
+    <Count />
   </StrictMode>,
 )
