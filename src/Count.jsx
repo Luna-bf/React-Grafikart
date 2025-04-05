a5dde0aa076d9c4113b1e54bd0b1035d486ec135
Luna-bf <lburattifayolle@gmail.com>
2025-04-05 14:55:04 +0200

    Added some comments to explain the useState hook

--- src/Count.jsx
+++ src/Count.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
+
 function Count() {
 
-    let count = 0;
+    {/*On a un tableau avec deux éléments :
+        - La valeur à incrementer (ici count)
+        - Un setter qui va nous permettre d'incrémenter cette valeur (ici setCount)
+    */}
+    const [count, setCount] = useState(0); {/*La valeur initiale de useState sera toujours zéro*/}
 
     const increment = () => {
 
