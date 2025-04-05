50077873aac0f8dbe05420c3471b69bd2dcb07ba
Luna-bf <lburattifayolle@gmail.com>
2025-04-05 16:38:06 +0200

    Added a form

--- src/Form.jsx
+++ src/Form.jsx
@@ -2,10 +2,24 @@ import { useState } from "react";
 
 function Form() {
 
-    return <>
+    const [value, setValue] = useState(''); {/*Je met une string vide pour ne pas avoir de problème*/}
     
-    
-    </>
+    const handleChange = (e) => {
+        setValue(e.target.value)
+    }
+
+
+    const [checked, setChecked] = useState(true)
+
+    const toggleCheck = () => {
+        setChecked(!checked)
+    }
+
+    return <form>
+        <textarea value={value} onChange={handleChange}/>
+        <input type="checkbox" checked={checked} onChange={toggleCheck}/>
+        <button disabled={!checked}>Envoyer</button>
+    </form>
 }
 
 export default Form
